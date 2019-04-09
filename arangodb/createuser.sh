5391ba46a14c4322002e1eba3bd398ec576c91b4
Guido Eco <guido.eco@crinstitute.org.uk>
2019-04-09 19:28:03 +0100

    update for new arangodb API

--- arangodb/createuser.sh
+++ arangodb/createuser.sh
@@ -12,48 +12,44 @@
 # get passwords and set default values for shell variables
 . ./ar-env.sh
 
-# create database, user and grant user read-write permissions to database 
-node <<@@EOF2
-sync = require('sync');
-request = require('request');
-function createdb(callback) {
-    Database = require('arangojs').Database;
-    db = new Database({url: 'http://root:${ARPASSWORD}@ar-server:8529', databaseName: '_system' });
-    db.listDatabases()
-    .then(names => { console.log('names: ', names);
-    db.createDatabase('${ARDBN}')
-    .then(info => { console.log('drop: ', info);
-		    db.listDatabases()
-    .then(names => { console.log('names: ', names)},
-          err => { console.error('names error: ', err)})}),
-    err => { console.error('error drop: ', err)}});
-}
-function createuser(callback) {
-    request.post('http://root:${ARPASSWORD}@${ARSVR}:8529/_api/user',
-		 { json: { 'user': '${ARUSR}', 'passwd': '${ARPWD}' }},
-		 function (error, response, body) {
-		     if (!error && response.statusCode == 200) {
-			    console.log(body)
-			}
-		 });
-};
-function grantpermissions(callback) {
-    request.put('http://root:${ARPASSWORD}@${ARSVR}:8529/_api/user/${ARUSR}/database/${ARDBN}',
-		{ json: { 'grant': 'rw' }},
-	     function (error, response, body) {
-		 if (!error && response.statusCode == 200) {
-			console.log(body)
-		    }
-	     });
-};
-sync(function(){
-	    createdb.sync(null);
-	});
-sync(function(){
-	    createuser.sync(null);
-	});
-sync(function(){
-	    grantpermissions.sync(null);
-	});
-@@EOF2
+USR=${1:-${ARUSR}}
 
+# Stop unless ~/.aqlpass file permissions are 0600
+if [ "x"$(stat -c %a ${HOME}/.aqlpass) != "x600" ]
+then
+    echo "Set permission on ~/.aqlpass to 0600 or nosuch file"
+    exit 1
+fi
+
+PWD=$(jq -r '.'${USR}' | select(. != null)' ${HOME}/.aqlpass)
+if [ "x"${PWD} = "x" ]
+then
+    echo "No password set in the .aqlpass file for user ${USR}"
+    exit 1
+fi
+
+# create user and grant user permissions (default 'r') to database
+PWR=${2:-'ro'}
+
+node <<@@EOF1
+const request = require('request');
+function createuser() {
+    return new Promise(function() {request.post('http://root:${ARPASSWORD}@${ARSVR}:8529/_api/user',
+		             { json: { 'user': '${USR}', 'passwd': '${ARPWD}' }},
+		             function (error, response, body) {
+		                 if (!error && response.statusCode == 200) { console.log(body); }
+                        if (error) { console.error(error); }
+		             })});
+};
+function grantpermissions() {
+    request.put('http://root:${ARPASSWORD}@${ARSVR}:8529/_api/user/${USR}/database/${ARDBN}',
+		            { json: { 'grant': '${PWR}' }},
+	              function (error, response, body) {
+		                if (!error && response.statusCode == 200) {
+			                     console.log(body)
+		                   }
+	              });
+};
+
+createuser().then(grantpermissions());
+@@EOF1
--- arangodb/dropuser.sh
+++ arangodb/dropuser.sh
@@ -11,14 +11,32 @@
 # get passwords and set default values for shell variables
 . ./ar-env.sh
 
+USR=${1:-${ARUSR}}
+
+if [ "x"${USR} = "x"${ARUSR} ]; then
+    echo "User is administrator ${ARUSR}"
+    exit 1
+fi
+
+# Stop unless ~/.aqlpass file permissions are 0600
+if [ "x"$(stat -c %a ${HOME}/.aqlpass) != "x600" ]
+then
+    echo "Set permission on ~/.aqlpass to 0600 or nosuch file"
+    exit 1
+fi
+
+PWD=$(jq -r '.'${USR}' | select(. != null)' ${HOME}/.aqlpass)
+if [ "x"${PWD} = "x" ]
+then
+    echo "No password set in the .aqlpass file for user ${USR}"
+    exit 1
+fi
+
 # drop user
 node <<@@EOF1
-sync = require('sync');
 request = require('request');
-function deleteuser(callback) {
-    request.delete('http://root:${ARPASSWORD}@${ARSVR}:8529/_api/user/${ARUSR}')
+function deleteuser() {
+    request.delete('http://root:${ARPASSWORD}@${ARSVR}:8529/_api/user/${USR}')
 }
-sync(function(){
-	    deleteuser.sync(null);
-	});
+deleteuser();
 @@EOF1
